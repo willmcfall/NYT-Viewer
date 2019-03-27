aa5f783d17064736466c82cd0714c1691ecf9cfc
unknown <wmcfall2@gmail.com>
2019-03-27 10:04:52 -0400

    fifth commit

--- assets/javascript/myjavascript.js
+++ assets/javascript/myjavascript.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
     // Creating a function that runs the AJAX call
     function displayArticles() {
       var api = "AJmum4IJuxlQ0gxxc2zp1rJOmn5FB1Uk";
+
       // Query
       var keywordFilter = $("#keywordFilter").val();
       console.log(keywordFilter);
+
       // Filter
-      var personFilter = $("#personFilter").val();
       var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?query=" + keywordFilter +"&api-key=" + api;
-  
+      console.log(queryURL);
+
       // Creating an AJAX call for the specific movie button being clicked
       $.ajax({
         url: queryURL,
@@ -21,7 +23,7 @@ $(document).ready(function() {
       };
 
     // Adding a click event listener for the click of the submit button
-    $("#submit").on("click", displayArticles());
+    $("#submit").on("click", displayArticles);
 
   });
   
