1f02b863a06eb10c176fe0a6bcf4738c5c3f70af
unknown <wmcfall2@gmail.com>
2019-03-27 13:05:59 -0400

    sixth commit

--- assets/javascript/myjavascript.js
+++ assets/javascript/myjavascript.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
     // Creating a function that runs the AJAX call
-    function displayArticles() {
+    function displayArticles(event) {
       var api = "AJmum4IJuxlQ0gxxc2zp1rJOmn5FB1Uk";
+
+      event.preventDefault();
 
       // Query
       var keywordFilter = $("#keywordFilter").val();
@@ -18,8 +20,24 @@ $(document).ready(function() {
         method: "GET"
       }).then(function(newspaper) {
         console.log(newspaper);
-        });
 
+      for (i=0; i< newspaper.response.docs.length; i++){
+        var newDiv = $("<div>");
+        var newPTitle = $("<h2>");
+        var newPParagraph = $("<p>");
+        var newALink = $("<a> Open Article </a>");
+        newPTitle.text(newspaper.response.docs[i].headline.main);
+        newPParagraph.text(newspaper.response.docs[i].lead_paragraph);
+        newALink.attr("href", newspaper.response.docs[i].web_url);
+        newALink.attr("target", "_blank");
+        $(newDiv).append(newPTitle);
+        $(newDiv).append(newPParagraph);
+        $(newDiv).append(newALink);
+        $(newDiv).append("<hr>");
+        $(".article_presentation").append(newDiv);
+      }
+
+        });
       };
 
     // Adding a click event listener for the click of the submit button
